b93862f960d52e58ac42a9d61bd6b3e4d3c980f1
mtsodf <mtsodf@gmail.com>
2024-07-24 23:51:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+# Printar algo no console
+print("Hello World!")
+
+# Printar texto em mais de uma linha
+print("Linha 1")
+print("Linha 2")
+print("Linha 1\nLinha 2")
+print("""Linha 1
+Linha 2
+""")
